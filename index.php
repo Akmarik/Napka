77dff05a8979e4529179aa120c62d9506f758bf6
Akmarik <dm_martych88@mail.ru>
2016-09-12 15:38:20 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en" ng-app="app">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AngularJS</title>
+    <script src="Libraries/angular.js"></script>
+
+    <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <!-- css -->
+    <link href="style/main.css" rel="stylesheet">
+    <link href="style/login.css" rel="stylesheet" />
+    <!-- end css -->
+    <script src="https://cdn.rawgit.com/angular-ui/ui-router/805e69bae319e922e4d3265b7ef565058aaff850/release/angular-ui-router.js"></script>
+    <script src="main.js"></script>
+</head>
+
+<body>
+<div ui-view></div>
+
+<script src="https://code.jquery.com/jquery-2.2.4.js" integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI=" crossorigin="anonymous"></script>
+<script src="bootstrap/js/jquery/jq.js"></script>
+<script src="bootstrap/js/bootstrap.js"></script>
+<script src="bootstrap/js/ui-bootstrap-tpls-2.1.3.min.js"></script>
+<script src="bootstrap/js/angular-cookies.js"></script>
+<script src="Libraries/ngCart/ngCart.js"></script>
+
+</body>
+
+</html>
